c7f4022d84f7d4520b216283dec742802609d675
Ridanss <pavlonk.04@spst.eu>
2023-06-06 11:29:58 +0200

    p05

---
+++ p05/Program5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p05
+{
+    internal static class Program5
+    {
+        /// <summary>
+        /// Hlavní vstupní bod aplikace.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
