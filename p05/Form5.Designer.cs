fdacf5dc89568714c82684245e722c8d8e19b012
Ridanss <pavlonk.04@spst.eu>
2023-06-12 08:44:49 +0200

    p05

--- p05/Form5.Designer.cs
+++ p05/Form5.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Location = new System.Drawing.Point(95, 82);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute.TabIndex = 0;
+            this.buttonExecute.Text = "vypsat";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 123);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 116);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonExecute);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- p05/Form5.cs
+++ p05/Form5.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,58 @@ namespace p05
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using(StreamReader sr = new StreamReader("sport.txt"))
+            {
+                string line;
+                string[] data;
+                FileStream fs = new FileStream("sport.dat", FileMode.Create, FileAccess.Write);
+                using(BinaryWriter br = new BinaryWriter(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        line = sr.ReadLine();
+                        data = line.Split(';');
+                        br.Write(int.Parse(data[0]));
+                        br.Write(data[1]);
+                        br.Write(data[2]);
+                        br.Write(char.Parse(data[3]));
+                        br.Write(int.Parse(data[4]));
+                        br.Write(int.Parse(data[5]));
+                    }
+                }
+            }
+        }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream("sport.dat", FileMode.Open, FileAccess.Read);
+                using(BinaryReader br = new BinaryReader(fs))
+                {
+                    while(br.BaseStream.Position < br.BaseStream.Length)
+                    {
+                        textBox1.Text += br.ReadInt32() + ";";
+                        textBox1.Text += br.ReadString() + ";";
+                        textBox1.Text += br.ReadString() + ";";
+                        textBox1.Text += br.ReadChar() + ";";
+                        textBox1.Text += br.ReadInt32() + ";";
+                        textBox1.Text += br.ReadInt32() + Environment.NewLine;
+                    }
+                }
+            }   
+            catch (FileNotFoundException)
+            {
+
+            }
+            catch (FormatException)
+            {
+
+            }
         }
     }
 }
